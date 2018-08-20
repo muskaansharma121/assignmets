7c95f4d6e95ba10ef5821b64466c4f8d11f583e1
muskaan <id>
2018-08-20 11:08:39 +0530

    first assignment

---
+++ ad.java
@@ -0,0 +1,13 @@
+import java .util.scanner;
+class ad
+{
+public static void main(string args[])
+{
+char input;
+scanner s1= neww scanner(system .in);
+system.out.printin("enter any char to print,$ to'exit');
+for( ;input!='$';)
+{
+input=
+}
+}
