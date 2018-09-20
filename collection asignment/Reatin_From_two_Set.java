40560e66539145108294aa4783ed169f1b3c3b76
muskaansharma121 <42472942+muskaansharma121@users.noreply.github.com>
2018-09-20 23:57:09 +0530

    Add files via upload

---
+++ collection asignment/Reatin_From_two_Set.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Reatin_From_two_Set{
+public static void main(String[] args){
+HashSet h1=new HashSet();
+h1.add(25);
+h1.add(56);
+h1.add(94);
+h1.add(21);
+h1.add(88);
+HashSet h2=new HashSet();
+h2.add(25);
+h2.add(22);
+h2.add(46);
+h2.add(88);
+h2.add(98);
+h1.retainAll(h2);
+System.out.println(h1);
+}
+}s
+
+
+
+
+
+
+
